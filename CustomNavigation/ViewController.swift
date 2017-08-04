12ad00f66554b5d5ec1935176b054388b3f4fc8f
Rajat Bhatt <r.bhatt@easternenterprise.com>
2017-08-04 15:16:47 +0530

    Initial Commit

---
+++ CustomNavigation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomNavigation
+//
+//  Created by Rajat Bhatt on 04/08/17.
+//  Copyright © 2017 Rajat Bhatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
