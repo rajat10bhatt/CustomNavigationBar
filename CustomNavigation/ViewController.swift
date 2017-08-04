e2c32ff80986aab9867d32ccbeb9a5f7ef66d9f5
Rajat Bhatt <r.bhatt@easternenterprise.com>
2017-08-04 17:32:28 +0530

    added flags to show or hide left and right bar button item

--- CustomNavigation/ViewController.swift
+++ CustomNavigation/ViewController.swift
@@ -17,35 +17,45 @@ class ViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        initializeAdminButtonNavigationBar(titleString: "Rajat Bhatt")
+        initializeAdminButtonNavigationBar(titleString: "Rajat Bhatt", isLeftBarButtonHidden: false, isRightBarButtonHidden: true)
     }
     
     /// Set navigation bar with Admin Button
-    func initializeAdminButtonNavigationBar(titleString: String) {
+    func initializeAdminButtonNavigationBar(titleString: String, isLeftBarButtonHidden: Bool, isRightBarButtonHidden: Bool) {
         let GAdminNavBGTintColor = UIColor(red: 53/255, green: 176/255, blue: 175/255, alpha: 1)
         navigationController?.navigationBar.barTintColor = GAdminNavBGTintColor
         navigationController?.navigationBar.isTranslucent = false
-        let backButtonImage : UIImage = UIImage(named: "ic_back")!
-        let imageFrame = CGRect(x: 0, y: 0, width: backButtonImage.size.width, height: backButtonImage.size.height)
-        let backButton = UIButton(frame: imageFrame)
-        backButton.setImage(backButtonImage, for: UIControlState.normal)
-        backButton.addTarget(self, action: #selector(navigateBack(sender:)), for: UIControlEvents.touchUpInside)
-        let barButton = UIBarButtonItem(customView: backButton)
-        navigationItem.setLeftBarButton(barButton, animated: true)
-        let cameraButtonImage : UIImage = UIImage(named: "ic_back")!
-        let cameraImageFrame = CGRect(x: 0, y: 0, width: cameraButtonImage.size.width, height: cameraButtonImage.size.height)
-        let cameraButton = UIButton(frame: cameraImageFrame)
-        cameraButton.setImage(cameraButtonImage, for: UIControlState.normal)
-        cameraButton.addTarget(self, action: #selector(onClickCameraBarButtonItem(sender:)), for: UIControlEvents.touchUpInside)
-        let cameraBarButton = UIBarButtonItem(customView: cameraButton)
-        navigationItem.setRightBarButton(cameraBarButton, animated: true)
+        //LeftBarButtonItem
+        if isLeftBarButtonHidden {
+            navigationItem.leftBarButtonItem = nil
+        } else {
+            let backButtonImage : UIImage = UIImage(named: "ic_back")!
+            let imageFrame = CGRect(x: 0, y: 0, width: backButtonImage.size.width, height: backButtonImage.size.height)
+            let backButton = UIButton(frame: imageFrame)
+            backButton.setImage(backButtonImage, for: UIControlState.normal)
+            backButton.addTarget(self, action: #selector(navigateBack(sender:)), for: UIControlEvents.touchUpInside)
+            let barButton = UIBarButtonItem(customView: backButton)
+            navigationItem.setLeftBarButton(barButton, animated: true)
+        }
+        //RightBarButtonItem
+        if isRightBarButtonHidden {
+            navigationItem.rightBarButtonItem = nil
+        } else {
+            let cameraButtonImage : UIImage = UIImage(named: "ic_back")!
+            let cameraImageFrame = CGRect(x: 0, y: 0, width: cameraButtonImage.size.width, height: cameraButtonImage.size.height)
+            let cameraButton = UIButton(frame: cameraImageFrame)
+            cameraButton.setImage(cameraButtonImage, for: UIControlState.normal)
+            cameraButton.addTarget(self, action: #selector(onClickCameraBarButtonItem(sender:)), for: UIControlEvents.touchUpInside)
+            let cameraBarButton = UIBarButtonItem(customView: cameraButton)
+            navigationItem.setRightBarButton(cameraBarButton, animated: true)
+        }
+        //TitleButton
         let adminButton = UIButton()
         adminButton.setTitle(titleString, for: .normal)
         adminButton.setTitleColor(GAdminNavBGTintColor, for: .normal)
         adminButton.frame = CGRect(x: 0, y: 0, width: 110, height: 50)
         adminButton.sizeToFit()
         adminButton.frame.size.width += 30
-        adminButton.layer.masksToBounds = true
         adminButton.layer.cornerRadius = adminButton.frame.size.height/2
         adminButton.backgroundColor = UIColor.white
         adminButton.addTarget(self, action: #selector(navigateBack(sender:)), for: UIControlEvents.touchUpInside)
